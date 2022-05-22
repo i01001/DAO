140080be7383f7da5f4edca5363e1f0540a500b2
Ikhlas <ikhlaskhan007@gmail.com>
2022-05-23 00:57:09 +0400

    May 23, 2022 12:57 AM

--- test/index.ts
+++ test/index.ts
@@ -54,32 +54,74 @@ describe("Testing the DAO Project Contract", () => {
   });
 
   describe("Checking DAO Token Contract deposit is working correctly", () => {
-    it("Checks the deposit function into the DAO", async () => {
-      await expect(dAO.connect(owner).deposit(1000)).to.be.revertedWith('approvalForDAOreq()');
+    it("Checks the deposit function in the DAO Project", async () => {
+      await expect(dAO.connect(owner).deposit(1000)).to.be.revertedWith(
+        "approvalForDAOreq()"
+      );
       await dAOT.connect(owner).approve(dAO.address, 1000);
       await dAO.connect(owner).deposit(1000);
       await expect(await dAOT.balanceOf(owner.address)).to.be.equal(9000);
       await expect(await dAOT.balanceOf(dAO.address)).to.be.equal(1000);
     });
-  })
 
-    // it("Checks the mint function of the TNETH Contract is working correctly or not", async () => {
-    //   await tNETH.mint(owner.address, 1000);
-    //     expect(await tNETH.balanceOf(owner.address)).to.be.equal(1000);
-    // })
+    it("Checks the withdraw function in the DAO Project", async () => {
+      await expect(dAO.connect(owner).withdraw(10000)).to.be.revertedWith(
+        "amountGreaterthanBalance(10000, 1000)"
+      );
+      await expect(dAO.connect(owner).withdraw(100));
+      await expect(await dAOT.balanceOf(owner.address)).to.be.equal(9100);
+      await expect(await dAOT.balanceOf(dAO.address)).to.be.equal(900);
+    });
 
-    // it("Checks the balance function of the TNBSC Contract is correct or not", async () => {
-    //     expect(await tNBSC.balanceOf(owner.address)).to.be.equal(0);
-    // })
+    it("Checks the New Proposal function in the DAO Project", async () => {
+      var jsonAbi = [
+        {
+          inputs: [],
+          name: "updatevalueoftemp",
+          outputs: [
+            {
+              "internalType": "uint256",
+              "name": "temporary",
+              "type": "uint256"
+              }
+          ],
+          stateMutability: "nonpayable",
+          type: "function",
+        },
+      ];
 
-    // it("Checks the updateChainById function of the Bridge Contract is working correctly or not", async () => {
-    //     expect(bC.connect(owner).updateChainById(31337, true)).to.be.revertedWith("true");
-    // })
+      const iface = new ethers.utils.Interface(jsonAbi);
+      const calldata = iface.encodeFunctionData("updatevalueoftemp", []);
+      const description = "Test Function counter";
 
-    // it("Checks the includeToken function of the Bridge Contract is working correctly or not", async () => {
-    //   expect(bC.connect(owner).includeToken(31337, "0x0000000000000000000000000000000000000000")).to.be.reverted;
-    //   expect(bC.connect(owner).includeToken(0, signertwo.address)).to.be.reverted;
-    //   expect(bC.connect(owner).includeToken(31337, tNETH.address)).to.be.revertedWith("true");
-    // })
- 
+      await expect(
+        dAO
+          .connect(signertwo)
+          .newProposal(calldata, testCall.address, description)
+      ).to.be.revertedWith("onlyChairPerson()");
+      await expect(
+        dAO.connect(owner).newProposal(calldata, testCall.address, description)
+      );
+      await expect(await dAO.proposalID).to.be.equal(1);
+    });
+  });
+
+  // it("Checks the mint function of the TNETH Contract is working correctly or not", async () => {
+  //   await tNETH.mint(owner.address, 1000);
+  //     expect(await tNETH.balanceOf(owner.address)).to.be.equal(1000);
+  // })
+
+  // it("Checks the balance function of the TNBSC Contract is correct or not", async () => {
+  //     expect(await tNBSC.balanceOf(owner.address)).to.be.equal(0);
+  // })
+
+  // it("Checks the updateChainById function of the Bridge Contract is working correctly or not", async () => {
+  //     expect(bC.connect(owner).updateChainById(31337, true)).to.be.revertedWith("true");
+  // })
+
+  // it("Checks the includeToken function of the Bridge Contract is working correctly or not", async () => {
+  //   expect(bC.connect(owner).includeToken(31337, "0x0000000000000000000000000000000000000000")).to.be.reverted;
+  //   expect(bC.connect(owner).includeToken(0, signertwo.address)).to.be.reverted;
+  //   expect(bC.connect(owner).includeToken(31337, tNETH.address)).to.be.revertedWith("true");
+  // })
 });
