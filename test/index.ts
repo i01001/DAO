700f0f37ac8b939db9be88c0d42190933a436ed5
Ikhlas <ikhlaskhan007@gmail.com>
2022-05-22 23:34:57 +0400

    May 22, 2022 11:34 PM

--- test/index.ts
+++ test/index.ts
@@ -1,7 +1,11 @@
-import { expect } from "chai";
-import { ethers } from "hardhat";
+import { expect } from 'chai';
+import { BigNumber } from 'bignumber.js';
+import { ethers, network} from 'hardhat';
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import {DAOProject, DAOToken,TestCalldata} from '../typechain'
+import "@nomiclabs/hardhat-web3";
 
-describe("Greeter", function () {
+describe("DAOProject", function () {
   it("Should return the new greeting once it's changed", async function () {
     const Greeter = await ethers.getContractFactory("Greeter");
     const greeter = await Greeter.deploy("Hello, world!");
