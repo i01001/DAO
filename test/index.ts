975fdc91bdbde64c4109b53416f46d3ce0aa80e2
Ikhlas <ikhlaskhan007@gmail.com>
2022-05-23 04:01:28 +0400

    May 23, 2022 4:01 AM

--- test/index.ts
+++ test/index.ts
@@ -191,7 +191,7 @@ describe("Testing the DAO Project Contract", () => {
       evm_increaseTime(3600);
       await dAO.connect(owner).endProposal(2);
       const status2 = await proposal2.status;
-      console.log(await status2);
+      console.log(status2);
       await expect(await proposal2.status).to.be.equal(2);
       await expect(await testCall.temporary()).to.be.equal(100);
     });
